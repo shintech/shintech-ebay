772dbae18f3c129e94d2bcb7d01ed0961d24d0e7
Michael Prather <mprather1@gmail.com>
2017-12-01 21:53:10 +0000

    new

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:6
+
+EXPOSE 8000:8000
+
+WORKDIR /shintech
+
+COPY . .
+
+RUN rm -rv node_modules build --force && \
+  printf "Creating file directories...\n" && \
+  mkdir build && \
+  wget -O /usr/local/bin/dumb-init https://github.com/Yelp/dumb-init/releases/download/v1.2.0/dumb-init_1.2.0_amd64 && \
+  chmod +x /usr/local/bin/dumb-init  
+
+RUN printf "Installing dependencies...\n" &&\
+  yarn install && \
+
+  printf "Building in progress...\nPlease wait...\n" && \
+  npm run build 
+
+CMD dumb-init npm start
---
+++ src/addItems.js
@@ -0,0 +1,75 @@
+import got from 'got'
+// import {parseString} from 'xml2js'
+import Handlebars from 'handlebars'
+import fs from 'fs'
+import path from 'path'
+
+const environment = process.env['NODE_ENV']
+const templates = path.join(path.dirname(__dirname), 'templates')
+const ebayEnvironments = {
+  development: 'https://api.sandbox.ebay.com/ws/api.dll',
+  production: 'https://api.ebay.com/ws/api.dll'
+}
+
+const opts = {
+  EBAY_API: process.env['EBAY_API'],
+  EBAY_APPID: process.env['EBAY_APPID'],
+  EBAY_DEVID: process.env['EBAY_DEVID'],
+  EBAY_CERTID: process.env['EBAY_CERTID'],
+  EBAY_TOKEN: process.env['EBAY_TOKEN'],
+  EMAIL: process.env['EMAIL']
+}
+
+export default function (raw) {
+  fs.readFile(path.join(templates, 'addItems.xml'), 'utf-8', (err, src) => {
+    if (err) return console.error(err)
+    Handlebars.registerHelper('messageID', (result) => {
+      return result['data'].index + 1
+    })
+
+    Handlebars.registerHelper('categoryID', (result) => {
+      return '13677'
+    })
+
+    while (raw.length) {
+      var products = raw.splice(0, 5)
+      const template = Handlebars.compile(src)
+      const output = template({
+        products: products,
+        opts: opts
+      })
+
+      sendXML(output, (err, response) => {
+        if (err) throw new Error(err)
+        console.log(response.body)
+      })
+    }
+  })
+}
+
+function sendXML (body, callback) {
+  got.post(ebayEnvironments[environment], {
+    method: 'POST',
+    body: body,
+    headers: getHeaders(opts)
+  })
+  .then(data => {
+    callback(null, data)
+  })
+  .catch(err => {
+    callback(err)
+  })
+}
+
+function getHeaders (opts) {
+  return {
+    'Content-Type': 'text/xml',
+    'X-EBAY-API-COMPATIBILITY-LEVEL': '967',
+    'X-EBAY-API-DEV-NAME': `${opts['EBAY_DEVID']}`,
+    'X-EBAY-API-APP-NAME': `${opts['EBAY_APPID']}`,
+    'X-EBAY-API-CERT-NAME': `${opts['EBAY_CERTID']}`,
+    'X-EBAY-API-SITEID': '0',
+    'X-EBAY-API-DETAIL-LEVEL': '0',
+    'X-EBAY-API-CALL-NAME': 'AddItems'
+  }
+}
